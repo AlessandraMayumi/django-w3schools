889ff63dc26adffe6db2feafe340c37da7804dc7
AlessandraMayumi <alessandra_mms@hotmail.com>
2024-08-07 18:03:10 -0300

    add a main page

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Member
 
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
 def members(request):
   mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
